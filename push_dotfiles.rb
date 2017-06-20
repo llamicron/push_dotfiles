0258212ad3c5f94a4d0e9dce3d5d74c82b505720
Luke Sweeney <luke@thesweeneys.org>
2017-06-20 14:40:10 -0500

    Adds try catch to check for things to push

--- push_dotfiles.rb
+++ push_dotfiles.rb
@@ -5,11 +5,18 @@ log = Dir.home + "/.push_dotfiles.log"
 
 File.open(log, "a") do |file|
   file.puts "\n---\n"
-  file.puts "Push at " + Time.now.to_s
-  file.puts dotfiles = Git.init(Dir.home + "/dotfiles/")
-  file.puts dotfiles.add(:all=>true)
-  file.puts dotfiles.commit_all("Automated push at " + Time.now.to_s)
-  file.puts dotfiles.push
-  file.puts "End of commit"
+  file.puts "Attempted push at " + Time.now.to_s
+
+  begin
+    dotfiles = Git.init(Dir.home + "/dotfiles/")
+    dotfiles.add(:all=>true)
+    dotfiles.commit_all("Automated push at " + Time.now.to_s)
+    dotfiles.push
+    file.puts "Push successful"
+  rescue Git::GitExecuteError
+    file.puts "Nothing to push"
+  end
+
+  file.puts "End of push"
 end
 
